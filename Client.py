efaaac0a54d1d6b97ee210a0dd35afa1f7aa8c4f
b0b0kiwi <kasumu.bolu@gmail.com>
2024-07-27 16:36:35 -0600

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,6 +5,44 @@ import threading
 HOST = "10.0.0.116"
 PORT = 1234
 
+def listen_for_messages_from_server(client):
+
+    while 1:
+
+        message = client.recv(2048).decode('utf-8')
+        if message != "":
+            username = message.split(":")[0]
+            content = message.split(":")[1]
+
+            print(f"[{username}] {content}")
+        else:
+            print("Message received from client is empty")
+
+def send_message_to_server(client):
+
+    while 1:
+
+        message= input("Message: ")
+        if message != "":
+            client.sendall(message.encode())
+        else:
+            print("Empty message")
+            exit(0)
+
+def communicate_to_server(client):
+
+    username = input("Enter username: ")
+    if username != "":
+        client.sendall(username.encode())
+    else:
+        print("Username cannot be empty")
+        exit(0)
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+
+    send_message_to_server(client)
+
+
 def main():
 
     # Creating a socket client
@@ -18,6 +56,10 @@ def main():
         print("Successfully Connected to Server!")
     except:
         print("Unable to Connect to Server: " + HOST + " " + str(PORT))
+
+    communicate_to_server(client)
+
+
 
 if __name__ == '__main__':
     main()
